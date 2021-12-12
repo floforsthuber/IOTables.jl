69039fd2f801636bec18d3155fd55a8dc4ef4540
Florian Forsthuber <florian.forsthuber@gmail.com>
2021-12-12 19:08:26 +0100

    reduce to EU+others

--- src/elasticities_functions.jl
+++ src/elasticities_functions.jl
@@ -4,24 +4,19 @@
 
 
 """
-    CP_statistic(X::Matrix, F::Matrix, τ_X::Matrix, τ_F::Matrix, N::Integer, S::Integer)
+    CP_statistic(X::Matrix, τ_X::Matrix, N::Integer, S::Integer)
 
-The function computes the LHS and RHS statistics of equation (23) in Caliendo and Parro (2015). Notice that intermediate and final demand 
-    are treated seperately with the goal in mind to obtain seperate trade elasticities for intermediate and final demand.
+The function computes the LHS and RHS statistics of equation (23) in Caliendo and Parro (2015).
 
 # Arguments
-- `X::Matrix`: NS×N, origin country-industry destination country intermediate demand matrix Z.
-- `F::Matrix`: NS×N, origin country-industry destination country final demand matrix F.
-- `τ_Z::Matrix`: NS×N, origin country-industry destination country intermediate input tariff matrix τ_Z.
-- `τ_F::Matrix`: NS×NS, origin country-industry destination country final demand tariff matrix τ_F.
+- `X::Matrix`: NS×N, origin country-industry destination country intermediate/final demand matrix X.
+- `τ_X::Matrix`: NS×N, origin country-industry destination country intermediate/final demand tariff matrix τ_X.
 - `N::Integer`: number of origin/destination countries.
 - `S::Integer`: number of origin/destination industries.
 
 # Output
-- `lhs_Z_temp::Vectpr{Float64}`: (Σᴺ⁻²n(n+1)/2)×1, LHS of eq. (23) in logs for intermediate inputs per industry.
-- `rhs_Z_temp::Vectpr{Float64}`: (Σᴺ⁻²n(n+1)/2)×1, RHS of eq. (23) in logs for intermediate inputs per industry.
-- `lhs_F_temp::Vectpr{Float64}`: (Σᴺ⁻²n(n+1)/2)×1, LHS of eq. (23) in logs for final demand per industry.
-- `rhs_F_temp::Vectpr{Float64}`: (Σᴺ⁻²n(n+1)/2)×1, RHS of eq. (23) in logs for final demand per industry.
+- `lhs_temp::Vectpr{Float64}`: (Σᴺ⁻²n(n+1)/2)×1, LHS of eq. (23) in logs for intermediate/final demand per industry.
+- `rhs_temp::Vectpr{Float64}`: (Σᴺ⁻²n(n+1)/2)×1, RHS of eq. (23) in logs for intermediate/final demand per industry.
 
 # Examples
 ```julia-repl
@@ -29,36 +24,26 @@ julia> lhs_Z_temp, rhs_Z_temp, lhs_F_temp, rhs_F_temp = create_reg_data(X_temp, 
 ```
 """
 
-function CP_statistic(X::Matrix, F::Matrix, τ_X::Matrix, τ_F::Matrix, N::Integer, S::Integer)
+function CP_statistic(X::Matrix, τ_X::Matrix, N::Integer, S::Integer)
 
     # initialize loop
-    LHS_Z = Float64[]
-    RHS_Z = Float64[]
-
-    LHS_F = Float64[]
-    RHS_F = Float64[]
+    LHS = Float64[]
+    RHS = Float64[]
 
     for i in 1:N-2 # i -> j
         for j in i+1:N-1 # j -> n
             for n in j+1:N # n -> i
-                push!( LHS_Z, ( X[i,j]*X[j,n]*X[n,i] ) / ( X[j,i]*X[n,j]*X[i,n] ) )
-                push!( RHS_Z, ( τ_X[i,j]*τ_X[j,n]*τ_X[n,i] ) / ( τ_X[j,i]*τ_X[n,j]*τ_X[i,n] ) )
-
-                push!( LHS_F, ( F[i,j]*F[j,n]*F[n,i] ) / ( F[j,i]*F[n,j]*F[i,n] ) )
-                push!( RHS_F, ( τ_F[i,j]*τ_F[j,n]*τ_F[n,i] ) / ( τ_F[j,i]*τ_F[n,j]*τ_F[i,n] ) )
+                push!( LHS, ( X[i,j]*X[j,n]*X[n,i] ) / ( X[j,i]*X[n,j]*X[i,n] ) )
+                push!( RHS, ( τ_X[i,j]*τ_X[j,n]*τ_X[n,i] ) / ( τ_X[j,i]*τ_X[n,j]*τ_X[i,n] ) )
             end
         end
     end
 
     # create series in logs
-    lhs_Z_temp = log.(LHS_Z)
-    rhs_Z_temp = log.(RHS_Z)
+    lhs_temp = log.(LHS)
+    rhs_temp = log.(RHS)
 
-    lhs_F_temp = log.(LHS_F)
-    rhs_F_temp = log.(RHS_F)
-
-
-    return lhs_Z_temp, rhs_Z_temp, lhs_F_temp, rhs_F_temp
+    return lhs_temp, rhs_temp
 end
 
 
@@ -66,24 +51,20 @@ end
 
 
 """
-    CP_statistic(Z::Matrix, F::Matrix, τ_Z::Matrix, τ_F::Matrix, N::Integer, S::Integer)
+    CP_statistic(M::Matrix, τ::Matrix, demand::String, N::Integer, S::Integer)
 
-A summary function to collect all industry LHS and RHS statistics of equation (23) in Caliendo and Parro (2015). Notice that intermediate and final demand 
-    are treated seperately.
+A summary function to collect all industry LHS and RHS statistics of equation (23) in Caliendo and Parro (2015). 
+    Notice that intermediate and final demand are treated seperately.
 
 # Arguments
-- `Z::Matrix`: NS×NS, origin country-industry destination country-industry intermediate demand matrix Z.
-- `F::Matrix`: NS×N, origin country-industry destination country final demand matrix F.
-- `τ_Z::Matrix`: NS×NS, origin country-industry destination country-industry intermediate input tariff matrix τ_Z.
-- `τ_F::Matrix`: NS×NS, origin country-industry destination country final demand tariff matrix τ_F.
+- `M::Matrix`: NS×NS, origin country-industry destination country-industry intermediate demand matrix Z.
+- `τ::Matrix`: NS×NS, origin country-industry destination country-industry intermediate input tariff matrix τ_Z.
 - `N::Integer`: number of origin/destination countries.
 - `S::Integer`: number of origin/destination industries.
 
 # Output
 - `lhs_Z::Vectpr{Float64}`: S*(Σᴺ⁻²n(n+1)/2)×1, LHS of eq. (23) in logs for intermediate inputs and all industries.
 - `rhs_Z::Vectpr{Float64}`: S*(Σᴺ⁻²n(n+1)/2)×1, RHS of eq. (23) in logs for intermediate inputs and all industries.
-- `lhs_F::Vectpr{Float64}`: S*(Σᴺ⁻²n(n+1)/2)×1, LHS of eq. (23) in logs for final demand and all industries.
-- `rhs_F::Vectpr{Float64}`: S*(Σᴺ⁻²n(n+1)/2)×1, RHS of eq. (23) in logs for final demand and all industries.
 
 # Examples
 ```julia-repl
@@ -91,58 +72,56 @@ julia> lhs_Z, rhs_Z, lhs_F, rhs_F = elasticity_data(Z, F, τ_Z, τ_F, N, S)
 ```
 """
 
-function elasticity_data(Z::Matrix, F::Matrix, τ_Z::Matrix, τ_F::Matrix, N::Integer, S::Integer)
+function elasticity_data(M::Matrix, τ::Matrix, demand::String, N::Integer, S::Integer)
 
-    # reduce intermediate demand to origin country-industry destination country
-    X = [sum(Z[i,j:j+S-1]) for i in 1:N*S, j in 1:S:N*S] # NS×N
-    #X = X .+ F
-
-    if size(τ_Z, 2) == N*S
-        τ_X = [mean(τ_Z[i,j:j+S-1]) for i in 1:N*S, j in 1:S:N*S] # NS×N
+    if demand == "intermediate"
+        # reduce intermediate demand to origin country-industry destination country
+        X = [sum(M[i,j:j+S-1]) for i in 1:N*S, j in 1:S:N*S] # NS×N
+    elseif demand == "total"
+        X = [sum(M[i,j:j+S-1]) for i in 1:N*S, j in 1:S:N*S] # NS×N
+        X = X .+ F
+    elseif demand == "final"
+        X = M
     else
-        τ_X = τ_Z
+        println(" × Failure! Specify demand schedule properly! \n")
+    end
+
+    if size(τ, 2) == N*S
+        τ_X = [mean(τ[i,j:j+S-1]) for i in 1:N*S, j in 1:S:N*S] # NS×N
+    else
+        τ_X = τ
     end
 
     # initialize loop
-    lhs_Z = Float64[]
-    rhs_Z = Float64[]
-
-    lhs_F = Float64[]
-    rhs_F = Float64[]
+    lhs = Float64[]
+    rhs = Float64[]
 
     for j in 1:S
 
         # same sector in rows
-        X_temp = X[j:S:(N-1)*S+j, :] # S×N
-        τ_X_temp = τ_X[j:S:(N-1)*S+j, :] # S×N
-        F_temp = F[j:S:(N-1)*S+j, :] # S×N
-        τ_F_temp = τ_F[j:S:(N-1)*S+j, :] # S×N
+        X_temp = X[j:S:(N-1)*S+j, :] # N×N
+        τ_X_temp = τ_X[j:S:(N-1)*S+j, :] # N×N
     
-        lhs_Z_temp, rhs_Z_temp, lhs_F_temp, rhs_F_temp = CP_statistic(X_temp, F_temp, τ_X_temp, τ_F_temp, N, S)
+        lhs_temp, rhs_temp = CP_statistic(X_temp, τ_X_temp, N, S)
 
-        lhs_Z = [lhs_Z; lhs_Z_temp]
-        rhs_Z = [rhs_Z; rhs_Z_temp]
-        lhs_F = [lhs_F; lhs_F_temp]
-        rhs_F = [rhs_F; rhs_F_temp]
+        lhs = [lhs; lhs_temp]
+        rhs = [rhs; rhs_temp]
         
     end
     
     # zero trade causes inf => create NaN instead which we let regression filter out later
-    lhs_Z = ifelse.(isinf.(lhs_Z), NaN, lhs_Z)
-    rhs_Z = ifelse.(isinf.(rhs_Z), NaN, rhs_Z)
-    
-    lhs_F = ifelse.(isinf.(lhs_F), NaN, lhs_F)
-    rhs_F = ifelse.(isinf.(rhs_F), NaN, rhs_F)
+    lhs = ifelse.(isinf.(lhs), NaN, lhs)
+    rhs = ifelse.(isinf.(rhs), NaN, rhs)
 
     # check if computed length equals number of (theoretical) combinations
     combinations = Int64(sum([n*(n+1)/2 for n in 1:N-2])) # number of possible combinations from formula
-    check = ([length(lhs_Z) length(rhs_Z) length(lhs_F) length(rhs_F)] .== S*combinations) == [1 1 1 1]
+    check = ([length(lhs) length(rhs)] .== S*combinations) == [1 1]
     if check == true 
         println(" ✓ The length of the vector coincides with the theoretical length of: $combinations \n")
     else
         println(" × Failure! The length of the vector does not! coincide with the theoretical length of: $combinations \n")
     end
 
-    return lhs_Z, rhs_Z, lhs_F, rhs_F
+    return lhs, rhs
  
 end
--- src/trade_elasticities_EU.jl
+++ src/trade_elasticities_EU.jl
@@ -43,15 +43,84 @@ df_tariffs = DataFrame(XLSX.readtable(dir * "MFN/tariff_matrix.xlsx", "Sheet1").
 
 # -------
 
-# use random tariff matrix for now
-τ_Z = 1 .+ rand(0.0:0.01:0.1, N*S, N*S) # NS×N
-τ_F = 1 .+ rand(0.0:0.01:0.1, N*S, N*S) # NS×N
-M = copy(transpose(Z))
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+# reduce to EU
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ctry_names_2013 = ["AUS", "AUT", "BEL", "BGR", "BRA", "CAN", "CHN", "CYP", "CZE", "DEU", "DNK", "ESP", "EST", "FIN", "FRA", "GBR", 
+    "GRC", "HUN", "IDN", "IND", "IRL", "ITA", "JPN", "KOR", "LTU", "LUX", "LVA", "MEX", "MLT", "NLD", "POL", "PRT", "ROM", "RUS", 
+    "SVK", "SVN", "SWE", "TUR", "TWN", "USA"]
+
+ctry_EU28 = ["AUT", "BEL", "BGR", "HRV", "CYP", "CZE", "DNK", "EST", "FIN", "FRA", "DEU", "GBR", "GRC", "HUN", "IRL", "ITA", "LVA", 
+    "LTU", "LUX", "MLT", "NLD", "POL", "PRT", "ROM", "SVK", "SVN", "ESP", "SWE"]
+
+countries = [ctry_names_2013; "ZoW"]
+
+position_EU28 = [2, 3, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 25, 26, 27, 29, 30, 31, 32, 33, 35, 36, 37]
+position_other = [1, 5, 6, 7, 19, 20, 23, 24, 28, 34, 38, 39, 40, 41]
+
+# -------------
+
+col_others = zeros(N*S, 1)
+col_EU = zeros(N*S, S)
+
+for j in 1:N
+    if j in position_EU28
+        col_EU += Z[:, 1+(j-1)*S:j*S]
+    else
+        col_others = hcat(col_others, Z[:, 1+(j-1)*S:j*S])
+    end
+end
+
+Z_temp = hcat(col_others[:,2:end], col_EU)
+
+row_others = zeros(1, size(Z_temp,2))
+row_EU = zeros(S, size(Z_temp,2))
+
+for i in 1:N
+    if i in position_EU28
+        row_EU += Z_temp[1+(i-1)*S:i*S, :]
+    else
+        row_others = vcat(row_others, Z_temp[1+(i-1)*S:i*S, :])
+    end
+end
+
+Z_new = vcat(row_others[2:end,:], row_EU)
+
+# -------------
+
+τ_Z_temp = hcat(τ_Z[:, position_other], τ_Z[:, 2])
+
+row_others = zeros(1, size(τ_Z_temp,2))
+row_EU = zeros(S, size(τ_Z_temp,2))
+
+for i in 1:N
+    if i in position_EU28
+        row_EU += τ_Z_temp[1+(i-1)*S:i*S, :]
+    else
+        row_others = vcat(row_others, τ_Z_temp[1+(i-1)*S:i*S, :])
+    end
+end
+
+row_EU = row_EU ./ length(position_EU28)
+
+τ_Z_new = vcat(row_others[2:end,:], row_EU)
 
 
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-lhs_Z, rhs_Z = elasticity_data(Z, τ_Z, "intermediate", N, S)
-lhs_F, rhs_F = elasticity_data(F, τ_F, "final", N, S)
+# Problem lies in the fact that I use MFN tariffs for every country, i.e.
+# rows are the same in τ_Z_new (i.e. same sectoral tariff applied to each country)
+# => need some variation!
+# => possibly do not need to reduce to EU matrix
+
+N = 15
+
+τ_Z_new2 = τ_Z_new .* (1 .+ rand(0.0:0.01:0.2, N*S, N))
+#τ_Z_new2 = 1 .+ rand(0.0:0.01:0.2, N*S, N*S) # NS×N
+
+lhs_Z, rhs_Z = elasticity_data(Z_new, τ_Z_new2, "intermediate", N, S)
 
 # -------
 
@@ -78,15 +147,13 @@ end
 FE_ctry = Int64.(repeat(FE_ctry, S)) # S*combinations×N
 
 # combine data in a DataFrame
-ctry_names_2013 = ["AUS", "AUT", "BEL", "BGR", "BRA", "CAN", "CHN", "CYP", "CZE", "DEU", "DNK", "ESP", "EST", "FIN", "FRA", "GBR", 
-    "GRC", "HUN", "IDN", "IND", "IRL", "ITA", "JPN", "KOR", "LTU", "LUX", "LVA", "MEX", "MLT", "NLD", "POL", "PRT", "ROM", "RUS", 
-    "SVK", "SVN", "SWE", "TUR", "TWN", "USA"]
-col_names = [["industry", "lhs_Z", "rhs_Z", "lhs_F", "rhs_F"]; ctry_names_2013; "RoW"]
+ctry_names = ["AUS", "BRA", "CAN", "CHN", "IDN", "IND", "JPN", "KOR", "MEX", "RUS", "TUR", "TWN", "USA"]
+col_names = [["industry", "lhs_Z", "rhs_Z"]; ctry_names; "RoW"; "EU"]
 
-df_reg = DataFrames.DataFrame([FE_ind lhs_Z rhs_Z lhs_F rhs_F FE_ctry], col_names)
+df_reg = DataFrames.DataFrame([FE_ind lhs_Z rhs_Z FE_ctry], col_names)
 
-# CSV.write(dir * "df_reg.csv", df_reg)
-
+CSV.write(dir * "df_reg.csv", df_reg)
+XLSX.writetable(dir * "df_reg.xlsx", df_reg, overwrite=true)
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -97,13 +164,5 @@ df_reg_Z = filter(:lhs_Z => x -> !(ismissing(x) || isnothing(x) || isnan(x)), df
 df_reg_Z = Float64.(df_reg_Z[:, [:lhs_Z, :rhs_Z]]) # columns need to have the right type!
 
 ols_Z = GLM.lm(@formula(lhs_Z ~ 0 + rhs_Z), df_reg_Z)
-
-# -------
-
-df_reg_F = filter(:lhs_F => x -> !(ismissing(x) || isnothing(x) || isnan(x)), df_reg)
-df_reg_F =  Float64.(df_reg_F[:, [:lhs_F, :rhs_F]]) # columns need to have the right type! 
-
-ols_F = GLM.lm(@formula(lhs_F ~ 0 + rhs_F), df_reg_F)
-
 
 
